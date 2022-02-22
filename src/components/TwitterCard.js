7e1dff39f9d9a82d4dbc857596c35acf743b1ee9
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2022-02-22 08:00:02 -0500

    moving retweet text

--- src/components/TwitterCard.js
+++ src/components/TwitterCard.js
@@ -5,12 +5,6 @@ export default function TwitterCard({ date, media, metrics, retweet, text }) {
       {retweet && (
         <div className="pt-1">
           <FaRetweet className="absolute -left-2 -top-2 md:-left-4 md:-top-4 text-green-400 w-6 h-6 md:w-10 md:h-10" />
-          <p className="absolute left-4 md:left-8 text-xs text-stone-400">
-            Retweeted from{" "}
-            <a href={`https://twitter.com/${retweet.username}`}>
-              {retweet.username}
-            </a>
-          </p>
         </div>
       )}
       {media ? (
@@ -37,12 +31,22 @@ export default function TwitterCard({ date, media, metrics, retweet, text }) {
           })}
         </div>
       ) : null}
-      <div
-        className="prose text-stone-100 md:prose-lg lg:prose-xl p-4 md:p-5 lg:p-7 "
-        dangerouslySetInnerHTML={{
-          __html: !retweet ? text : retweet[0].text,
-        }}
-      ></div>
+      <div className="relative">
+        {retweet && (
+          <p className="absolute left-4 md:left-8 text-xs text-stone-400">
+            Retweeted from{" "}
+            <a href={`https://twitter.com/${retweet.username}`}>
+              {retweet.username}
+            </a>
+          </p>
+        )}
+        <div
+          className="prose text-stone-100 md:prose-lg lg:prose-xl p-4 md:p-5 lg:p-7 "
+          dangerouslySetInnerHTML={{
+            __html: !retweet ? text : retweet[0].text,
+          }}
+        ></div>
+      </div>
       <div className="text-xs text-center pb-2 md:pb-3 lg:pb-4">
         {retweet ? `Retweeted on ` : `Tweeted on `}
         {new Date(date).toLocaleDateString("en-US", {
