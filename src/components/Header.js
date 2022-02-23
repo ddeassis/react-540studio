cb9b2d85c89564b3e094ee994501b21b35defb3a
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2022-02-23 11:40:27 -0500

    adjusting styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import logo from "../images/studio-logo.png";
 import Headroom from "react-headroom";
 export default function Header() {
   return (
-    <Headroom style={{ background: "rgba(28, 25, 23, 0.95)" }}>
-      <div className="py-3 md:py-4 flex flex-col justify-center items-center shadow-sm shadow-green-500">
+    <Headroom>
+      <div className="py-3 md:py-4 flex flex-col justify-center items-center shadow-xl shadow-white">
         <img src={logo} className="w-16 h-16 md:w-24 md:h-24" alt="" />
         <p className="text-4xl font-londrina font-bold text-white">
           @lb540studio
