4b4ff5f19a3aa43a82c620732d818797049a1412
ddeassis <95487623+ddeassis@users.noreply.github.com>
2022-02-23 10:55:05 -0500

    moving site title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Headroom from "react-headroom";
 export default function Header() {
   return (
     <Headroom style={{ background: "rgba(28, 25, 23, 0.95)" }}>
-      <div className="py-3 md:py-4 flex justify-center items-center shadow-sm shadow-green-500">
+      <div className="py-3 md:py-4 flex flex-col justify-center items-center shadow-sm shadow-green-500">
         <img src={logo} className="w-16 h-16 md:w-24 md:h-24" alt="" />
         <p className="text-4xl font-londrina font-bold text-white">
           @lb540studio
