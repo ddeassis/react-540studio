1762f0c215b9a3be26fe7bb48c368c2b1e39840b
ddeassis <95487623+ddeassis@users.noreply.github.com>
2022-03-03 15:36:02 -0500

    changing shadow

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import Headroom from "react-headroom";
 export default function Header() {
   return (
     <Headroom>
-      <div className="py-3 md:py-4 flex flex-col justify-center items-center shadow-s shadow-black">
+      <div className="py-3 md:py-4 flex flex-col justify-center items-center shadow-sm shadow-black">
         <img src={logo} className="w-16 h-16 md:w-24 md:h-24" alt="" />
-        <p className="text-4xl font-londrina font-bold text-white">
+        <p className="text-4xl font-londrina text-white">
           @lb540studio
         </p>
       </div>
