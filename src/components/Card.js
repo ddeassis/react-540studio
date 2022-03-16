be1433adeff06341697e6439a4ee0a9462ac480f
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2022-03-16 12:37:26 -0400

    increase z-index on hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card({ children }) {
   return (
-    <div className="bg-stone-50 dark:bg-stone-900 rounded shadow transition duration-500 ease-in-out lg:hover:scale-105 relative">
+    <div className="bg-stone-50 dark:bg-stone-900 rounded shadow transition duration-500 ease-in-out lg:hover:scale-105 hover:z-20 relative">
       {children}
     </div>
   );
